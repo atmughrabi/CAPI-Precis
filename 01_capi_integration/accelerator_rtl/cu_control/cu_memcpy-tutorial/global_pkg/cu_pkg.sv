1416f3b07c341191d41625be781cb472283c405d
Abdullah Mughrabi <atmughra@ncsu.edu>
2020-07-15 16:08:49 -0400

    [RTL] add read engine still buggy

--- 01_capi_integration/accelerator_rtl/cu_control/cu_memcpy-tutorial/global_pkg/cu_pkg.sv
+++ 01_capi_integration/accelerator_rtl/cu_control/cu_memcpy-tutorial/global_pkg/cu_pkg.sv
@@ -23,9 +23,6 @@ package CU_PKG;
         READ_STREAM_IDLE,
         READ_STREAM_SET,
         READ_STREAM_START,
-        READ_STREAM_REQ,
-        READ_STREAM_PENDING,
-        READ_STREAM_DONE,
         READ_STREAM_FINAL
     } read_state;
 
@@ -36,6 +33,36 @@ package CU_PKG;
         WRITE_DATA
     } array_struct_type;
 
+    // Read/write commands require the size to be a power of 2 (1, 2, 4, 8, 16, 32, 64, 128).
+    function logic [0:11] cmd_size_calculate(logic [0:(ARRAY_SIZE_BITS-1)]  num_counter);
+
+        logic [0:(ARRAY_SIZE_BITS-1)] num_size;
+        logic [0:11] request_size;
+
+        num_size = (num_counter << $clog2(ARRAY_SIZE));
+
+        if (num_size > 64)
+            request_size = 128;
+        else if (num_size > 32)
+            request_size = 64;
+        else if (num_size > 16)
+            request_size = 32;
+        else if (num_size > 8)
+            request_size = 16;
+        else if (num_size > 4)
+            request_size = 8;
+        else if (num_size > 2)
+            request_size = 4;
+        else if (num_size > 1)
+            request_size = 2;
+        else if (num_size > 0)
+            request_size = 1;
+        else
+            request_size = 0;
+
+        return request_size;
+
+    endfunction : cmd_size_calculate
     
 
 endpackage
--- 01_capi_integration/accelerator_rtl/cu_control/cu_memcpy-tutorial/memcpy-tutorial/cu/read_engine.sv
+++ 01_capi_integration/accelerator_rtl/cu_control/cu_memcpy-tutorial/memcpy-tutorial/cu/read_engine.sv
@@ -20,6 +20,7 @@ module read_engine #(parameter CU_READ_CONTROL_ID = DATA_READ_CONTROL_ID) (
 
 	logic              enabled_in                        ;
 	WEDInterface       wed_request_in_latched            ;
+	WEDInterface       wed_request_in_driver             ;
 	ResponseBufferLine read_response_in_latched          ;
 	ReadWriteDataLine  read_data_0_in_latched            ;
 	ReadWriteDataLine  read_data_1_in_latched            ;
@@ -27,6 +28,11 @@ module read_engine #(parameter CU_READ_CONTROL_ID = DATA_READ_CONTROL_ID) (
 
 	CommandBufferLine             read_command_out_latched     ;
 	logic [0:(ARRAY_SIZE_BITS-1)] read_job_counter_done_latched;
+
+
+	logic        cmd_setup    ;
+	logic        send_cmd_read;
+	logic [0:63] next_offset  ;
 
 ////////////////////////////////////////////////////////////////////////////
 //drive input logic
@@ -72,16 +78,18 @@ module read_engine #(parameter CU_READ_CONTROL_ID = DATA_READ_CONTROL_ID) (
 ////////////////////////////////////////////////////////////////////////////
 //drive out logic
 ////////////////////////////////////////////////////////////////////////////
-	assign read_command_out_latched      = 0;
-	assign read_job_counter_done_latched = 0;
+	// assign read_command_out_latched      = 0;
+	// assign read_job_counter_done_latched = 0;
 
 	always_ff @(posedge clock or negedge rstn) begin
 		if(~rstn) begin
 			read_command_out.valid <= 0;
 			read_job_counter_done  <= 0;
 		end else begin
-			read_command_out.valid <= read_command_out_latched.valid;
-			read_job_counter_done  <= read_job_counter_done_latched;
+			if(enabled_in) begin
+				read_command_out.valid <= read_command_out_latched.valid;
+				read_job_counter_done  <= read_job_counter_done_latched;
+			end
 		end
 	end
 
@@ -93,9 +101,130 @@ module read_engine #(parameter CU_READ_CONTROL_ID = DATA_READ_CONTROL_ID) (
 //read state machine
 ////////////////////////////////////////////////////////////////////////////
 
+	read_state current_state, next_state;
 
+	always_ff @(posedge clock or negedge rstn) begin
+		if(~rstn)
+			current_state <= READ_STREAM_RESET;
+		else begin
+			if(enabled_in)
+				current_state <= next_state;
+		end
+	end// always_ff @(posedge clock)
 
+	always_comb begin
+		next_state = current_state;
+		case (current_state)
+			READ_STREAM_RESET : begin
+				next_state = READ_STREAM_IDLE;
+			end
+			READ_STREAM_IDLE : begin
+				if(wed_request_in_latched.valid && enabled_in)
+					next_state = READ_STREAM_SET;
+				else
+					next_state = READ_STREAM_IDLE;
+			end
+			READ_STREAM_SET : begin
+				next_state = READ_STREAM_START;
+			end
+			READ_STREAM_START : begin
+				if(|wed_request_in_driver.payload.wed.size_send)
+					next_state = READ_STREAM_START;
+				else
+					next_state = READ_STREAM_FINAL;
+			end
+			READ_STREAM_FINAL : begin
+				next_state = READ_STREAM_FINAL;
+			end
+		endcase
+	end
 
+	always_ff @(posedge clock) begin
+		case (current_state)
+			READ_STREAM_RESET : begin
+				cmd_setup     <= 0;
+				send_cmd_read <= 0;
+			end
+			READ_STREAM_IDLE : begin
+				cmd_setup <= 0;
+			end
+			READ_STREAM_SET : begin
+				cmd_setup <= 1;
+			end
+			READ_STREAM_START : begin
+				cmd_setup     <= 0;
+				send_cmd_read <= 1;
+			end
+			READ_STREAM_FINAL : begin
+				send_cmd_read <= 0;
+			end
+		endcase
+	end
+
+////////////////////////////////////////////////////////////////////////////
+//response tracking logic
+////////////////////////////////////////////////////////////////////////////
+
+	always_ff @(posedge clock or negedge rstn) begin
+		if(~rstn)
+			read_job_counter_done_latched <= 0;
+		else begin
+			if (read_response_in_latched.valid) begin
+				read_job_counter_done_latched <= read_job_counter_done_latched + read_response_in_latched.payload.cmd.real_size;
+			end
+		end
+	end
+
+////////////////////////////////////////////////////////////////////////////
+//read command driver
+////////////////////////////////////////////////////////////////////////////
+
+	always_ff @(posedge clock or negedge rstn) begin
+		if(~rstn) begin
+			read_command_out_latched <= 0;
+			next_offset              <= 0;
+			wed_request_in_driver    <= 0;
+		end else begin
+
+			if(cmd_setup)
+				wed_request_in_driver <= wed_request_in_latched;
+
+			if (~read_command_buffer_status_latched.alfull && (|wed_request_in_driver.payload.wed.size_send) && send_cmd_read)begin
+
+				if(wed_request_in_driver.payload.wed.size_send > CACHELINE_ARRAY_NUM)begin
+					wed_request_in_driver.payload.wed.size_send          <= wed_request_in_driver.payload.wed.size_send - CACHELINE_ARRAY_NUM;
+					read_command_out_latched.payload.cmd.real_size       <= CACHELINE_ARRAY_NUM;
+					read_command_out_latched.payload.cmd.real_size_bytes <= 12'h080;
+
+					read_command_out_latched.payload.size    <= 12'h080;
+					read_command_out_latched.payload.command <= READ_CL_NA;
+				end else if (wed_request_in_driver.payload.wed.size_send <= CACHELINE_ARRAY_NUM) begin
+					wed_request_in_driver.payload.wed.size_send          <= 0;
+					read_command_out_latched.payload.cmd.real_size       <= wed_request_in_driver.payload.wed.size_send;
+					read_command_out_latched.payload.cmd.real_size_bytes <= cmd_size_calculate(wed_request_in_driver.payload.wed.size_send);
+
+					read_command_out_latched.payload.size    <= cmd_size_calculate(wed_request_in_driver.payload.wed.size_send);
+					read_command_out_latched.payload.command <= READ_PNA;
+				end
+
+				read_command_out_latched.payload.cmd.cu_id_x          <= CU_READ_CONTROL_ID;
+				read_command_out_latched.payload.cmd.cu_id_y          <= CU_READ_CONTROL_ID;
+				read_command_out_latched.payload.cmd.cmd_type         <= CMD_READ;
+				read_command_out_latched.payload.cmd.cacheline_offset <= 0;
+				read_command_out_latched.payload.cmd.address_offset   <= next_offset;
+				read_command_out_latched.payload.cmd.array_struct     <= READ_DATA;
+				read_command_out_latched.payload.cmd.abt              <= STRICT;
+
+				read_command_out_latched.valid           <= 1'b1;
+				read_command_out_latched.payload.abt     <= STRICT; // cmd order
+				read_command_out_latched.payload.address <= wed_request_in_driver.payload.wed.array_send + next_offset;
+				next_offset                              <= next_offset + CACHELINE_SIZE;
+
+			end else begin
+				read_command_out_latched <= 0;
+			end
+		end
+	end
 
 
 endmodule 
